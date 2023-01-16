5dcf39d55780faec0752f1d85035f05fd11da0b6
jonadls <jona13_metalml@hotmail.com>
2023-01-16 11:39:03 -0300

    fusionamos el menu mobile, agregamos mediaqueries del menu

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const email = document.querySelector(".navbar-email");
-const desktopmenu = document.querySelector(".desktop-menu")
+const desktopmenu = document.querySelector(".desktop-menu");
+
+const mobilemenu = document.querySelector(".mobile-menu");
+const iconmenumobile = document.querySelector(".menu");
 
 email.addEventListener("click", menucompras);
 
+iconmenumobile.addEventListener("click", iconocategorias);
+
 function menucompras() {
     desktopmenu.classList.toggle("inactivo")
+}
+
+
+function iconocategorias() {
+    mobilemenu.classList.toggle("inactivo")
 }
