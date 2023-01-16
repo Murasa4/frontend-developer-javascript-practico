c589438f7cbddaca0108536921543b16210c59fb
jonadls <jona13_metalml@hotmail.com>
2023-01-16 13:49:02 -0300

    agregamos el menu del icono del carrito de compras

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
+//correo icon
 const email = document.querySelector(".navbar-email");
 const desktopmenu = document.querySelector(".desktop-menu");
 
+//menu icon
 const mobilemenu = document.querySelector(".mobile-menu");
 const iconmenumobile = document.querySelector(".menu");
 
-email.addEventListener("click", menucompras);
+//carrito icon
+const carritoicon = document.querySelector(".navbar-shopping-cart")
+const carritocompras = document.querySelector(".product-detail");
+
+email.addEventListener("click", menucorreo);
 
 iconmenumobile.addEventListener("click", iconocategorias);
 
-function menucompras() {
-    desktopmenu.classList.toggle("inactivo")
-}
+carritoicon.addEventListener("click", compras);
 
 
+//funcion del menu del correo
+function menucorreo() {
+    desktopmenu.classList.toggle("inactivo");
+    mobilemenu.classList.add("inactivo");
+    carritocompras.classList.add("inactivo");
+};
+
+//funcion del icono del menu movil
 function iconocategorias() {
-    mobilemenu.classList.toggle("inactivo")
+    mobilemenu.classList.toggle("inactivo");
+    carritocompras.classList.add("inactivo");
+    desktopmenu.classList.add("inactivo");
+};
+
+//funcion del icono del carrito
+function compras() {
+    carritocompras.classList.toggle("inactivo");
+    mobilemenu.classList.add("inactivo");
+    desktopmenu.classList.add("inactivo");
 }
